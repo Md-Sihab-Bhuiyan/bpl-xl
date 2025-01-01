4a08ce8cf818f7a37425b238ca5a60b6745e6fee
Md. Sihab Bhuiyan <sihab.bhuiyan68690@gmail.com>
2025-01-01 19:12:06 +0600

    check

--- src/App.jsx
+++ src/App.jsx
@@ -8,49 +8,52 @@ function App() {
 
   return (
     <>
-      <nav className="flex items-center justify-between bg-white text-gray-800 p-4">
-        {/* Left Side: Logo */}
-        <div className="flex items-center">
-          <img
-            src="/assets/logo.png"
-            alt="Logo"
-            className="h-10 w-10 mr-4"
-          />
-          <span className="text-xl font-bold hidden">BPL</span>
-        </div>
 
-        {/* Right Side: Links and Button */}
-        <div className="flex items-center space-x-6">
-          <a href="#home" className="hover:text-blue-400">
-            Home
-          </a>
-          <a href="#fixture" className="hover:text-blue-400">
-            Fixture
-          </a>
-          <a href="#teams" className="hover:text-blue-400">
-            Teams
-          </a>
-          <a href="#schedule" className="hover:text-blue-400">
-            Schedule
-          </a>
+      <div className='bg-white'>
+        <nav className="flex items-center justify-between bg-white text-gray-800 p-4">
+          {/* Left Side: Logo */}
+          <div className="flex items-center">
+            <img
+              src="/assets/logo.png"
+              alt="Logo"
+              className="h-10 w-10 mr-4"
+            />
+            <span className="text-xl font-bold hidden">BPL</span>
+          </div>
 
-          {/* Coin Button */}
-          <button className="flex items-center bg-white-600 border-black px-4 py-2 rounded hover:bg-blue-500">
-          <span>0 Coins</span>
-          <img src="/public/assets/icons8-coin-16.png" className="h-6 w-6 mr-2"/>
-          </button>
+          {/* Right Side: Links and Button */}
+          <div className="flex items-center space-x-6">
+            <a href="#home" className="hover:text-blue-400">
+              Home
+            </a>
+            <a href="#fixture" className="hover:text-blue-400">
+              Fixture
+            </a>
+            <a href="#teams" className="hover:text-blue-400">
+              Teams
+            </a>
+            <a href="#schedule" className="hover:text-blue-400">
+              Schedule
+            </a>
+
+            {/* Coin Button */}
+            <button className="flex items-center bg-white-600 border-black px-4 py-2 rounded hover:bg-blue-500">
+              <span>0 Coins</span>
+              <img src="/public/assets/icons8-coin-16.png" className="h-6 w-6 mr-2" />
+            </button>
+          </div>
+        </nav>
+        <Header></Header>
+        <div className='flex justify-between pt-5 px-5'>
+          <p className='text-3xl'>Available Players </p>
+          <div>
+            <button className='btn btn-warning'>Available</button>
+            <button className='btn'>Selected()</button>
+          </div>
         </div>
-      </nav>
-      <Header></Header>
-      <div className='flex justify-between pt-5 px-5'>
-                <p className='text-3xl'>Available Players </p>
-                <div>
-                <button className='btn btn-warning'>Available</button>
-                <button className='btn'>Selected()</button>
-                </div>
-            </div>
-      <Players></Players>
-      <Footer></Footer>
+        <Players></Players>
+        <Footer></Footer>
+      </div>
     </>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             }}
         >
             <div className="flex justify-center items-center w-full">
-                <img src="/public/assets/banner-main.png" alt="Banner" />
+                <img src="/assets/banner-main.png" alt="Banner" />
             </div>
             <h6 className="footer-title mt-4">Assemble Your Ultimate Dream 11 Cricket Team</h6>
             <h1 className="text-xl mt-2">Beyond Boundaries Beyond Limits</h1>
